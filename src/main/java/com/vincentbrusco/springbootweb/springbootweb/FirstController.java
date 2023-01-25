2476572ce9589530f847cf69b78657d9f4ce34ea
Vincent Brusco <vbrusco30794@gmail.com>
2023-01-25 15:17:45 -0500

    first commit

---
+++ src/main/java/com/vincentbrusco/springbootweb/springbootweb/FirstController.java
@@ -0,0 +1,22 @@
+package com.vincentbrusco.springbootweb.springbootweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+
+public class FirstController {
+
+    @RequestMapping(path = "/")
+    public String indexPage()
+    {
+        return "index";
+    }
+
+    @RequestMapping(path = "/submit")
+    public String submitPage()
+    {
+        return "submit";
+    }
+    
+}
